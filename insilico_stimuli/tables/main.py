23378caa55c57b3ef83d44a861462f9cdd46eefc
Paweł <ppierzc@gmail.com>
2020-12-10 18:56:44 +0100

    [REFACTOR] remove unnecessary comment

--- insilico_stimuli/tables/main.py
+++ insilico_stimuli/tables/main.py
@@ -151,15 +151,3 @@ class StimuliOptimizeMethod(dj.Lookup):
             method_config[key] = attr
 
         return method_config
-
-# class OptimisedStimuliTemplate(OptimisedStimuliTemplateMixin, dj.Computed):
-#     """Stimuli table template.
-#     To create a functional "Stimuli" table, create a new class that inherits from this template and decorate it with your
-#     preferred Datajoint schema. Next assign your trained model (or trained ensemble model) and your selector table to
-#     the class variables called "trained_model_table" and "selector_table". By default, the created table will point to
-#     the "StimuliMethod" table in the Datajoint schema called "nnfabrik.main". This behavior can be changed by overwriting
-#     the class attribute called "method_table".
-#     """
-#
-#     optimisation_method_table = StimuliOptimizeMethod
-#     StimulusSet_table = InsilicoStimuliSet
