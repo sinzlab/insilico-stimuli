[FIX] gradient descent to use fixed_norm instead of std

insilico_stimuli/optimizers/gradient_descent.py
@@ -3,8 +3,8 @@ from nnidentify.fitgabor.fitgabor import GaborGenerator, trainer_fn
 import torch
 
 
-def gradient_descent_per_unit(StimulusSet, gabor_gen, model, unit, epochs, lr, fixed_std):
-    gabor_gen, _ = trainer_fn(gabor_gen, lambda x: model(x)[:, unit], epochs=epochs, lr=lr, fixed_std=fixed_std)
+def gradient_descent_per_unit(StimulusSet, gabor_gen, model, unit, epochs, lr, fixed_norm):
+    gabor_gen, _ = trainer_fn(gabor_gen, lambda x: model(x)[:, unit], epochs=epochs, lr=lr, fixed_norm=fixed_norm)
 
     max_img = gabor_gen()
     max_activation = model(max_img.cuda()).detach().cpu().numpy().squeeze()[unit]
@@ -23,11 +23,11 @@ def gradient_descent_per_unit(StimulusSet, gabor_gen, model, unit, epochs, lr, f
     return stimuli_params, max_activation
 
 
-def gradient_descent(StimulusSet, model, unit=None, seed=None, fixed_std=1., lr=5e-3, epochs=20000):
+def gradient_descent(StimulusSet, model, key, unit=None, seed=None, fixed_norm=10, lr=5e-3, epochs=20000):
     torch.manual_seed(seed)
 
     if unit is None:
-        gabor_gen = GaborGenerator(StimulusSet.canvas_size[::-1], target_std=fixed_std)
+        gabor_gen = GaborGenerator(StimulusSet.canvas_size[::-1], target_norm=fixed_norm)
         test_img = gabor_gen()
         activations = model(test_img.cuda()).detach().cpu().numpy().squeeze()
 
@@ -35,9 +35,9 @@ def gradient_descent(StimulusSet, model, unit=None, seed=None, fixed_std=1., lr=
         max_activations = []
 
         for _unit in range(len(activations)):
-            gabor_gen = GaborGenerator(StimulusSet.canvas_size[::-1], target_std=fixed_std)
+            gabor_gen = GaborGenerator(StimulusSet.canvas_size[::-1], target_norm=fixed_norm)
             stimuli_params, max_activation = gradient_descent_per_unit(StimulusSet, gabor_gen, model, _unit, epochs, lr,
-                                                                       fixed_std)
+                                                                       fixed_norm)
 
             max_params.append(stimuli_params)
             max_activations.append(max_activation)
@@ -45,8 +45,8 @@ def gradient_descent(StimulusSet, model, unit=None, seed=None, fixed_std=1., lr=
         return max_params, max_activations
 
     else:
-        gabor_gen = GaborGenerator(StimulusSet.canvas_size[::-1], target_std=fixed_std)
+        gabor_gen = GaborGenerator(StimulusSet.canvas_size[::-1], target_norm=fixed_norm)
         stimuli_params, max_activation = gradient_descent_per_unit(StimulusSet, gabor_gen, model, unit, epochs, lr,
-                                                                   fixed_std)
+                                                                   fixed_norm)
 
         return stimuli_params, max_activation